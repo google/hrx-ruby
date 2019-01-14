645bda70dbda29cf95f7c2cffb8fd36f6ee2ffd4
alkesh26 <alkesh26@gmail.com>
2019-01-14 14:11:35 +0530

    Ruby standard guide: Using single quoted string when no interpolation or special characters required.

--- spec/executable/create_spec.rb
+++ spec/executable/create_spec.rb
@@ -12,26 +12,26 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-RSpec.describe "hrx create", type: :aruba do
+RSpec.describe 'hrx create', type: :aruba do
   before :each do
-    write_file("file1.txt", "contents 1\n")
-    write_file("file2.txt", "contents 2\n")
+    write_file('file1.txt', "contents 1\n")
+    write_file('file2.txt', "contents 2\n")
 
-    write_file("sub/file1.txt", "sub contents 1\n")
-    write_file("sub/file2.txt", "sub contents 2\n")
-    write_file("sub/.file.txt", "sub dot contents\n")
+    write_file('sub/file1.txt', "sub contents 1\n")
+    write_file('sub/file2.txt', "sub contents 2\n")
+    write_file('sub/.file.txt', "sub dot contents\n")
   end
 
-  it "creates an empty HRX file" do
-    run_simple "bin/hrx create archive.hrx"
+  it 'creates an empty HRX file' do
+    run_simple 'bin/hrx create archive.hrx'
 
-    expect("archive.hrx").to have_file_content("")
+    expect('archive.hrx').to have_file_content('')
   end
 
-  it "creates an HRX file with the given contents" do
-    run_simple "bin/hrx create archive.hrx file1.txt file2.txt"
+  it 'creates an HRX file with the given contents' do
+    run_simple 'bin/hrx create archive.hrx file1.txt file2.txt'
 
-    expect("archive.hrx").to have_file_content <<END
+    expect('archive.hrx').to have_file_content <<END
 <===> file1.txt
 contents 1
 
@@ -40,10 +40,10 @@ contents 2
 END
   end
 
-  it "creates an HRX file with everything in a directory" do
-    run_simple "bin/hrx create archive.hrx sub"
+  it 'creates an HRX file with everything in a directory' do
+    run_simple 'bin/hrx create archive.hrx sub'
 
-    expect("archive.hrx").to have_file_content <<END
+    expect('archive.hrx').to have_file_content <<END
 <===> sub/.file.txt
 sub dot contents
 
@@ -55,10 +55,10 @@ sub contents 2
 END
   end
 
-  it "writes filenames relative to --root" do
-    run "bin/hrx create --root sub archive.hrx sub"
+  it 'writes filenames relative to --root' do
+    run 'bin/hrx create --root sub archive.hrx sub'
 
-    expect("archive.hrx").to have_file_content <<END
+    expect('archive.hrx').to have_file_content <<END
 <===> .file.txt
 sub dot contents
 
@@ -70,43 +70,43 @@ sub contents 2
 END
   end
 
-  context "fails gracefully for" do
+  context 'fails gracefully for' do
     it "an input file that doesn't exist" do
-      run "bin/hrx create archive.hrx no-file.txt", fail_on_error: false
+      run 'bin/hrx create archive.hrx no-file.txt', fail_on_error: false
       expect(last_command_started).not_to be_successfully_executed
       expect(last_command_started.stderr).not_to include_a_stack_trace
     end
 
     it "an archive in a directory that doesn't exist" do
-      run "bin/hrx create no-dir/archive.hrx file1.txt", fail_on_error: false
+      run 'bin/hrx create no-dir/archive.hrx file1.txt', fail_on_error: false
       expect(last_command_started).not_to be_successfully_executed
       expect(last_command_started.stderr).not_to include_a_stack_trace
     end
 
     it "an input file that's not in the working directory" do
-      cd "sub"
+      cd 'sub'
 
-      run "bin/hrx create archive.hrx ../file1.txt", fail_on_error: false
+      run 'bin/hrx create archive.hrx ../file1.txt', fail_on_error: false
       expect(last_command_started).not_to be_successfully_executed
       expect(last_command_started.stderr).not_to include_a_stack_trace
     end
 
     it "an input file that's not in the root directory" do
-      run "bin/hrx create --root sub archive.hrx file1.txt", fail_on_error: false
+      run 'bin/hrx create --root sub archive.hrx file1.txt', fail_on_error: false
       expect(last_command_started).not_to be_successfully_executed
       expect(last_command_started.stderr).not_to include_a_stack_trace
     end
 
     it "an input file that's not valid UTF-8" do
-      write_file "invalid-file.txt", "\xc3\x28\n".b
-      run "bin/hrx create archive.hrx invalid-file.txt", fail_on_error: false
+      write_file 'invalid-file.txt', "\xc3\x28\n".b
+      run 'bin/hrx create archive.hrx invalid-file.txt', fail_on_error: false
       expect(last_command_started).not_to be_successfully_executed
       expect(last_command_started.stderr).not_to include_a_stack_trace
     end
 
-    it "an input file with an invalid path" do
-      touch "foo:bar.txt"
-      run "bin/hrx create archive.hrx foo:bar.txt", fail_on_error: false
+    it 'an input file with an invalid path' do
+      touch 'foo:bar.txt'
+      run 'bin/hrx create archive.hrx foo:bar.txt', fail_on_error: false
       expect(last_command_started).not_to be_successfully_executed
       expect(last_command_started.stderr).not_to include_a_stack_trace
     end
